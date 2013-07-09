a608009da71956a7119309cbe92f07e9d10f1132
Denis Zavgorodny <denis.zavgorodny@gmail.com>
2013-07-10 00:06:41 +0300

    + add screenshot

--- create-content/create-content.js
+++ create-content/create-content.js
@@ -3,26 +3,73 @@ module.exports = function(grunt) {
     grunt.registerMultiTask('createContent', 'Create content for html files', function() {
         var done = this.async();
         var path = require('path');
+        var webshot = require('/usr/local/lib/node_modules/grunt/node_modules/webshot');
+        var optionsWebshot = {
+          siteType: 'url',
+          timeout: 10000,
+          screenSize: {
+            width: 1024,
+            height: 768
+          },
+            shotSize: {
+            width: 1024,
+            height: 'all'
+          },
+            userAgent: 'Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_2 like Mac OS X; en-us)'
+            + ' AppleWebKit/531.21.20 (KHTML, like Gecko) Mobile/7B298g'
+        };
+
         var fs = require('fs');
         this.requiresConfig([ this.name, this.target, 'contentFileName' ].join('.'));
         this.requiresConfig([ this.name, this.target, 'basePath' ].join('.'));
+        this.requiresConfig([ this.name, this.target, 'wwwPath' ].join('.'));
 
         this.data.basePath = this.data.basePath || process.env.OLDPWD;
+        this.data.wwwPath = this.data.wwwPath || '';
 
         var linkList = [];
+        var fileShot = [];
         var linkTemplate = '<li><a href="%url%" target="_blank">%name%</a></li>';
         var that = this;
         fs.readdir(that.data.basePath, function(error, files){
+            if (!fs.readdirSync(that.data.basePath+'/thumb/'))
+                fs.mkdirSync(that.data.basePath+'/thumb/', '0777');
             for(var key in files) {
                 var _file = files[key].split('.');
-                if (_file[1] == 'html' && files[key] != that.data.contentFileName)
-                    linkList.push(linkTemplate.replace(/%name%/g, files[key]).replace(/%url%/g, files[key]));
+                if (!that.data.wwwPath)
+                    optionsWebshot.siteType = 'html';
+                if (_file[1] == 'html' && files[key] != that.data.contentFileName) {
+                    linkList.push(linkTemplate.replace(/%name%/g, true?"<h2>"+files[key]+"</h2><img src='"+'thumb/'+_file[0]+'.png'+"' />": files[key]).replace(/%url%/g, files[key]));
+                    fileShot.push({
+                        from: that.data.wwwPath?that.data.wwwPath+files[key]:that.data.basePath+'/'+files[key],
+                        to: that.data.basePath+'/thumb/'+_file[0]+'.png'
+                    });
+                    /*(function(from, to){
+                        webshot(from, to, optionsWebshot, function(err) {
+                            if (err)
+                                console.log(err);
+                            else
+                                console.log(from+'... OK');
+                        });        
+                    })(that.data.wwwPath?that.data.wwwPath+files[key]:that.data.basePath+'/'+files[key], that.data.basePath+'/thumb/'+_file[0]+'.png');*/
+                    
+                }    
             }
             var fileHTMLContent = "<!DOCTYPE html >\
                                     <html>\
                                         <head>\
                                             <meta http-equiv='content-type' content='text/html; charset=utf-8' />\
-                                            <style></style>\
+                                            <style>\
+                                                ul > li img {\
+                                                    width: 100%;\
+                                                    display: block;\
+                                                }\
+                                                ul > li { \
+                                                    display:-moz-inline-box; display:inline-block; *zoom: 1; *display:inline; vertical-align: top; \
+                                                    width: 320px; \
+                                                    margin: 10px; \
+                                                } \
+                                            </style>\
                                         </head>\
                                         <body>\
                                             <ul>\
@@ -34,10 +81,26 @@ module.exports = function(grunt) {
                 if (err) throw err;
                 console.log('It\'s saved!');
             });
+
+
+            //Стартуем скриншоты
+            createShot(fileShot.pop());
         });
-            
+        function createShot(shot) {
+            webshot(shot.from, shot.to, optionsWebshot, function(err) {
+                if (err)
+                    console.log(err);
+                else
+                    console.log(shot.from+'... OK');
+                if (fileShot.length)
+                    createShot(fileShot.pop());
+            });        
+        }
+
         return;
         
         
     });
+
+    
 };
